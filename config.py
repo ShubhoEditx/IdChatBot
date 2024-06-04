af9e14250565dd2f153d444309515b008914f94d
ShubhoEditx <166376289+ShubhoEditx@users.noreply.github.com>
2024-06-04 11:59:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,18 +14,18 @@ API_HASH = getenv("API_HASH", None)
 STRING_SESSION = getenv("STRING_SESSION", None)
 
 ### ❖ ➥
-OWNER_ID = int(getenv("OWNER_ID", "6922271843"))
+OWNER_ID = int(getenv("OWNER_ID", "7146599240"))
 
 ### ❖ ➥
 MONGO_URL = getenv("MONGO_URL", None)
 
 ### ❖ ➥
-SUPPORT_GRP = getenv("SUPPORT_GRP", "THE_FRIENDZ")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "SHUBHO_CREATIONS")
 
 ### ❖ ➥
-UPDATE_CHNL = getenv("UPDATE_CHNL", "ROY_EDITX")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "ABOUT_SHUBHO")
 
 ### ❖ ➥
-OWNER_USERNAME = getenv("OWNER_USERNAME", "LOCO_X_PILOT")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "HELLO_DEEAR")
 
 
